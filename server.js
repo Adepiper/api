851c7a4cc6a9149c24039ad56e8da75c4ae40f48
opsz2 <titilope2@hotmail.com>
2019-11-20 21:20:17 +0100

    This should solve cross origin issue

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const express = require('express'),
   cors = require('cors'),
   mongoose = require('mongoose'),
   config = require('./db'),
-  userRoutes = require('./user/user.route')
-  routes = require('./index.route'),
-  session = require('express-session')
- 
+  userRoutes = require('./user/user.route');
+(routes = require('./index.route')), (session = require('express-session'));
+
 mongoose.Promise = global.Promise;
 const app = express();
 let port = process.env.PORT || 3000;
@@ -16,42 +15,33 @@ app.use(cors());
 
 require('./user/passport');
 
-
 app.set('view engine', 'ejs');
-app.use('/public', express.static('public'))
+app.use('/public', express.static('public'));
 
-app.use('/user', (req, res, next) => 
-{
-	res.setHeader('Content-Type', 'application/json');
-	res.setHeader('Access-Control-Allow-Origin', req.header('Origin') || '*');
-	res.setHeader('Access-Control-Allow-Headers', '*');
-	res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS,PATCH");
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	res.status(200);
-	next();
-})
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use((err, req, res, next) => {
-  console.log(err, res)
-  res.status(err.status || 500)
+  console.log(err, res);
+  res.status(err.status || 500);
 
   res.json({
     errors: {
       message: err.message,
       error: {}
     }
-  })
-})
-app.use('/', (req, res, next) => 
-{
-	res.setHeader('Content-Type', 'application/json');
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', '*');
-	res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS,PATCH");
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	res.status(200);
-	next();
-})
+  });
+});
 
 app.use('/', routes);
 app.use('/user', userRoutes);
